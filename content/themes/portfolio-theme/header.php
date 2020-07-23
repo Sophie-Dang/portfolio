409938b4383408d5959ad6101d944229ae301d3f
Sophie-Dang <sophie.dang.kieuduyen@gmail.com>
2020-07-23 17:27:30 +0200

    add google analytics

--- content/themes/portfolio-theme/header.php
+++ content/themes/portfolio-theme/header.php
@@ -5,6 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Sophie Dang - Développeuse web fullstack avec spécialisation au CMS Wordpress. Vous trouverez ici toutes mes compétences et les projets personnels réalisés jusqu'à ce jour.">
   <?php wp_head(); ?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-173408875-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-173408875-1');
+  </script>
+
 </head>
 <body>
   
