d5edaebaa3edf25dd4fb1499e52432fd7795779b
Sophie-Dang <sophie.dang.kieuduyen@gmail.com>
2020-10-20 00:35:09 +0200

    clear folders and gitignore

--- content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"0^[jIS<D`q*8D-6$7d[2hdn%C;AM4X=%*yMw~vZe3M/P!A{6~#+z7WBS.d=M2PP5";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:3719;}
